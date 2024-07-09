002e5080b9d5f67f4c112659b5369484b353a64e
kemelite1 <osikemelite42@gmail.com>
2024-07-09 21:17:29 +0100

    Display hero and products

--- timbu/timbushop/src/pages/HomePage.jsx
+++ timbu/timbushop/src/pages/HomePage.jsx
@@ -1,15 +1,11 @@
 import AllProducts from "../components/AllProducts"
 import Hero from "../components/Hero"
-import RecommendedProducts from "../components/RecommendedProducts"
-import EmptyCartPage from "./EmptyCartPage"
 
 const HomePage = () => {
   return (
     <>
     <Hero />
     <AllProducts />
-    <EmptyCartPage />
-    <RecommendedProducts />
     </>
   )
 }
