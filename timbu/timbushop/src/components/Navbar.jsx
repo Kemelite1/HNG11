82c9ef4c006e542423a57a6e714d3c2b4b71daf5
kemelite1 <osikemelite42@gmail.com>
2024-07-09 13:24:43 +0100

    Navbar component

--- timbu/timbushop/src/components/Navbar.jsx
+++ timbu/timbushop/src/components/Navbar.jsx
@@ -1,9 +1,32 @@
-
+import { FaUser, FaSearch, FaShoppingCart } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="px-[88px] py-[30px] bg-[#FFFFFF] flex justify-between items-center">
+      <a href="#" className="w-[154px] h-[22px] text-[18px] text-primary font-plus-jakarta-sans font-bold leading-[21.6px]">
+        Timbu cloud shop
+      </a>
+      <div className="hidden md:flex space-x-6">
+        <a href="#" className="text-[18px] font-plus-jakarta-sans font-semibold">All Products</a>
+        <a href="#" className="text-[18px] font-plus-jakarta-sans font-semibold">Men</a>
+        <a href="#" className="text-[18px] font-plus-jakarta-sans font-semibold">Women</a>
+        <a href="#" className="text-[18px] font-plus-jakarta-sans font-semibold">Kids</a>
+      </div>
+      
+      <div className="flex space-x-3">
+        <a href="#" className="p-3">
+          <FaSearch />
+        </a>
+        <a href="#" className="p-3">
+          <FaUser />
+        </a>
+        <a href="#" className="p-3">
+          <FaShoppingCart />
+        </a>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
+
